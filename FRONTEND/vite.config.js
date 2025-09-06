0a939d2cb47468a9f349bfbf23b2f23532c08f78
WorstGen <47105856+WorstGen@users.noreply.github.com>
2025-09-06 13:59:51 -0700

    Update vite.config.js

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Root directory
   server: {
     port: 3000,
   },
