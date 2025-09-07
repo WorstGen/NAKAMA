ef0556ddbb4333aab479a8f4ef9057d8d0ff525e
WorstGen <47105856+WorstGen@users.noreply.github.com>
2025-09-07 02:59:03 -0700

    Update vite.config.js

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -3,17 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   build: {
     outDir: 'build',
-  },
-  optimizeDeps: {
-    include: [
-      '@solana/wallet-adapter-base',
-      '@solana/wallet-adapter-react',
-      '@solana/wallet-adapter-react-ui',
-      '@solana/wallet-adapter-wallets',
-      '@solana/web3.js',
-    ],
+    rollupOptions: {
+      input: 'index.html',
+    },
   },
 })
