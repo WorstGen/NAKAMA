bcd203a9ed4013286d8a4731f3374b6dd4923509
WorstGen <47105856+WorstGen@users.noreply.github.com>
2025-09-07 03:12:39 -0700

    Update vite.config.js

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -4,7 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: false,
   build: {
     outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    },
+  },
+  server: {
+    port: 3000,
   },
 })
